b4927f3b9166b9bb79cb902f82002ab58708e73d
Icaro-Lima <icarodantas.lima@gmail.com>
2020-06-28 01:04:05 -0300

    Better title and add card

--- src/pages/ContributeTutorial.js
+++ src/pages/ContributeTutorial.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React from 'react';
+import {getTheme, mergeStyles} from '@fluentui/react'
+// import Editor from "rich-markdown-editor";
 
 export default function ContributeTutorial() {
+    const theme = getTheme();
+
+    const titleClass = mergeStyles({
+        marginTop: theme.spacing.l2,
+        marginLeft: theme.spacing.l2,
+        marginBottom: theme.spacing.l2,
+        color: theme.semanticColors.bodyText,
+    }, theme.fonts.xxLarge);
+
+    const cardClass = mergeStyles({
+        height: '200px',
+        marginLeft: theme.spacing.l2,
+        marginRight: theme.spacing.l2,
+        boxShadow: theme.effects.elevation4,
+        backgroundColor: theme.semanticColors.bodyFrameBackground
+    })
+
     return (
-        <div>
-            <h2>Contribute a Tutorial</h2>
-        </div>
+        <>
+            <div className={titleClass}>Contribute a Tutorial</div>
+            <div className={cardClass}>
+            </div>
+        </>
     );
 }
