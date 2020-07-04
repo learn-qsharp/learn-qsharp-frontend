3c2e32812f935ed68daca56f36be630d8e47a469
Icaro-Lima <icarodantas.lima@gmail.com>
2020-07-04 18:49:06 -0300

    Add basic columns

--- src/pages/Tutorials.js
+++ src/pages/Tutorials.js
@@ -1,6 +1,21 @@
 import React from "react";
 import PageTitle from "../components/PageTitle";
+import {DetailsList} from "@fluentui/react"
+import {SelectionMode} from "@fluentui/react"
 
 export default function Tutorials() {
-    return <PageTitle>Tutorials</PageTitle>;
+    const columns = [
+        {key: 'id', name: '#', fieldName: 'id', minWidth: 16, maxWidth: 16},
+        {key: 'title', name: 'Title', fieldName: 'title'},
+        {key: 'tags', name: 'Tags', fieldName: 'tags'},
+        {key: 'author', name: 'Author', fieldName: 'author'},
+        {key: 'difficulty', name: 'Difficulty', fieldName: 'difficulty'},
+    ];
+
+    return (
+        <>
+            <PageTitle>Tutorials</PageTitle>
+            <DetailsList columns={columns} items={[]} selectionMode={SelectionMode.none}/>
+        </>
+    );
 }
