cea5afa853ed12684f12072709433f0e83f82a0e
Icaro-Lima <icarodantas.lima@gmail.com>
2020-07-05 10:23:08 -0300

    Show tutorials

--- src/pages/Tutorials.js
+++ src/pages/Tutorials.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import PageTitle from "../components/PageTitle";
 import {DetailsList} from "@fluentui/react"
 import {SelectionMode} from "@fluentui/react"
 
 export default function Tutorials() {
+    const [items, setItems] = useState([]);
+    // const [hasError, setHasError] = useState(false);
+    // const [error, setError] = useState('');
+
     const columns = [
         {key: 'id', name: '#', fieldName: 'id', minWidth: 16, maxWidth: 16},
         {key: 'title', name: 'Title', fieldName: 'title'},
@@ -12,10 +16,35 @@ export default function Tutorials() {
         {key: 'difficulty', name: 'Difficulty', fieldName: 'difficulty'},
     ];
 
+    useEffect(() => {
+        fetch(process.env.REACT_APP_API_URL + '/tutorials')
+            .then((response) => {
+                return response.json()
+            })
+            .then((tutorials) => {
+                tutorials.forEach((tutorial, i) => {
+                    const item = {
+                        key: tutorial.id,
+                        id: tutorial.id,
+                        title: tutorial.title,
+                        tags: tutorial.tags,
+                        author: tutorial.author,
+                        difficulty: tutorial.difficulty,
+                    }
+
+                    setItems(items => [...items, item])
+                })
+            })
+        // .catch((err) => {
+        // setError(err)
+        // setHasError(true)
+        // })
+    }, [])
+
     return (
         <>
             <PageTitle>Tutorials</PageTitle>
-            <DetailsList columns={columns} items={[]} selectionMode={SelectionMode.none}/>
+            <DetailsList columns={columns} items={items} selectionMode={SelectionMode.none}/>
         </>
     );
 }
